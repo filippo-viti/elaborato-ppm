00fd76aa8f5d8ebe89597993040a690edc45568b
Filippo Viti <filippo.viti2002@gmail.com>
2024-05-11 18:22:45 +0200

    Styled navbar

---
+++ src/components/buttons/IconButton.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const IconButton = styled.button`
+    display: flex;
+    color: black;
+    width: 40px;
+    height: 40px;
+    text-align: center;
+    align-items: center;
+    justify-content: center;
+    border: none;
+    background-color: transparent;
+    cursor: pointer;
+`;
--- src/components/header/HeaderNavbar.tsx
+++ src/components/header/HeaderNavbar.tsx
@@ -1,15 +1,63 @@
 import styled from "styled-components";
+import {IconButton} from "../buttons/IconButton.tsx";
 
 const Navbar = styled.div`
     display: flex;
     align-items: center;
+    justify-content: space-between;
+    width: inherit;
+
+    ${IconButton} {
+        padding: 13px 8px 13px 14px;
+        margin-right: .5rem;
+    }
 `;
+
+const Nav = styled.nav`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Ul = styled.ul`
+    display: flex;
+    flex-wrap: nowrap;
+    list-style-type: none;
+`;
+
+const Li = styled.li`
+    margin: .5rem 1.5rem .5rem 0;
+`;
+
+const ButtonGroup = styled.div`
+    display: flex;
+    padding: 0 14px 0 0;
+    align-items: center;
+`;
+
+const LiveButtonWrapper = styled.div`
+    padding: 0 0 0 .9rem;
+    display: inline-block;
+    font-size: 10px;
+    line-height: 20px;
+    text-transform: uppercase;
+`;
+
+const LiveButtonBadge = styled.svg`
+    width: 20px;
+    margin-right: .2rem;
+`;
+
+const A = styled.a`
+    color: #d61116;
+`;
+
 
 export default function HeaderNavbar() {
   return (
     <Navbar>
       <HamburgerButton/>
-      <NavLinks/>
+      <NavbarLinks/>
       <NavRightButtons/>
     </Navbar>
   );
@@ -17,62 +65,88 @@ export default function HeaderNavbar() {
 
 function HamburgerButton() {
   return (
-    <button>
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-list"
-           viewBox="0 0 16 16">
-        <path fillRule="evenodd"
-              d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+    <IconButton>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 14">
+        <path d="M0 12h24v2H0zM0 6h24v2H0zM0 0h24v2H0z"></path>
       </svg>
-    </button>
+    </IconButton>
   );
 }
 
-function NavLinks() {
+function NavbarLinks() {
   return (
-    <nav>
-      <a href={"#"}>Lorem ipsum</a>
-      <a href={"#"}>Lorem ipsum</a>
-      <a href={"#"}>Lorem ipsum</a>
-      <a href={"#"}>Lorem ipsum</a>
-      <a href={"#"}>Lorem ipsum</a>
-      <a href={"#"}>Lorem ipsum</a>
-      <a href={"#"}>Lorem ipsum</a>
-      <a href={"#"}>Lorem ipsum</a>
-      <a href={"#"}>Lorem ipsum</a>
-    </nav>
+    <Nav>
+      <Ul>
+        <Li><a href={"#"}>Lorem ipsum</a></Li>
+        <Li><a href={"#"}>Lorem ipsum</a></Li>
+        <Li><a href={"#"}>Lorem ipsum</a></Li>
+        <Li><a href={"#"}>Lorem ipsum</a></Li>
+        <Li><a href={"#"}>Lorem ipsum</a></Li>
+        <Li><a href={"#"}>Lorem ipsum</a></Li>
+        <Li><a href={"#"}>Lorem ipsum</a></Li>
+        <Li><a href={"#"}>Lorem ipsum</a></Li>
+        <Li><a href={"#"}>Lorem ipsum</a></Li>
+      </Ul>
+    </Nav>
   );
 }
 
 function NavRightButtons() {
   return (
-    <div>
+    <ButtonGroup>
       <NotificationsButton/>
       <LiveButton/>
-    </div>
+    </ButtonGroup>
   );
 }
 
 function NotificationsButton() {
   return (
-    <button>
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-bell"
-           viewBox="0 0 16 16">
-        <path
-          d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2M8 1.918l-.797.161A4 4 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4 4 0 0 0-3.203-3.92zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5 5 0 0 1 13 6c0 .88.32 4.2 1.22 6"/>
+    <IconButton>
+      <svg xmlns="http://www.w3.org/2000/svg" width="15" height="17" viewBox="0 0 15 17" fill="white">
+        <g clipPath="url(#clip0_401_3269)">
+          <path
+            d="M13.64 9.42C13.05 8.91 12.72 8.18 12.72 7.4V5.22C12.72 2.34 10.38 0 7.5 0C4.62 0 2.28 2.34 2.28 5.22V7.4C2.28 8.18 1.94 8.91 1.36 9.42C0.5 10.17 0 11.25 0 12.39C0 13.14 0.61 13.75 1.36 13.75H13.64C14.39 13.75 15 13.14 15 12.39C15 11.25 14.51 10.17 13.64 9.42ZM13.1 10.07C13.77 10.66 14.15 11.5 14.15 12.39C14.15 12.67 13.92 12.9 13.64 12.9H1.36C1.08 12.9 0.85 12.67 0.85 12.39C0.85 11.49 1.24 10.64 1.92 10.06C2.69 9.39 3.14 8.42 3.14 7.4V5.22C3.13 2.81 5.09 0.85 7.5 0.85C9.91 0.85 11.87 2.81 11.87 5.22V7.4C11.87 8.42 12.31 9.39 13.1 10.07Z"
+            fill="#555555"></path>
+          <path
+            d="M9.65001 14.05H5.35001C5.20001 14.05 5.06001 14.12 4.96001 14.24C4.87001 14.36 4.83001 14.5 4.86001 14.65C5.10001 15.78 6.21001 16.59 7.50001 16.59C8.79001 16.59 9.89001 15.77 10.13 14.64C10.16 14.5 10.13 14.35 10.03 14.23C9.93001 14.11 9.79001 14.04 9.64001 14.04L9.65001 14.05ZM9.14001 14.9C8.83001 15.41 8.20001 15.75 7.51001 15.75C6.82001 15.75 6.18001 15.42 5.87001 14.9H9.15001H9.14001Z"
+            fill="#555555"></path>
+        </g>
+        <defs>
+          <clipPath id="clip0_401_3269">
+            <rect width="15" height="16.59" fill="white"></rect>
+          </clipPath>
+        </defs>
       </svg>
-    </button>
+    </IconButton>
   );
 }
 
 function LiveButton() {
   return (
-    <a>
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-circle-fill"
-           viewBox="0 0 16 16">
-        <circle cx="8" cy="8" r="8"/>
-      </svg>
-      Live
-    </a>
+    <LiveButtonWrapper>
+      <A>
+        <LiveButtonBadge xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+          <g clipPath="url(#clip0_832_5029)">
+            <path opacity="0.9"
+                  d="M10 -0.00244141C4.477 -0.00244141 0 4.47506 0 9.99756C0 15.5201 4.477 19.9976 10 19.9976C15.523 19.9976 20 15.5206 20 9.99756C20 4.47456 15.523 -0.00244141 10 -0.00244141ZM10 17.5081C5.852 17.5081 2.4895 14.1456 2.4895 9.99756C2.4895 5.84956 5.852 2.48706 10 2.48706C14.148 2.48706 17.5105 5.84956 17.5105 9.99756C17.5105 14.1456 14.148 17.5081 10 17.5081Z"
+                  fill="#D61116"></path>
+            <path opacity="0.5"
+                  d="M10 2.48706C5.852 2.48706 2.4895 5.84956 2.4895 9.99756C2.4895 14.1456 5.852 17.5081 10 17.5081C14.148 17.5081 17.5105 14.1456 17.5105 9.99756C17.5105 5.84956 14.148 2.48706 10 2.48706ZM10 14.7611C7.369 14.7611 5.2365 12.6286 5.2365 9.99756C5.2365 7.36656 7.369 5.23406 10 5.23406C12.631 5.23406 14.7635 7.36656 14.7635 9.99756C14.7635 12.6286 12.631 14.7611 10 14.7611Z"
+                  fill="#D61116"></path>
+            <path opacity="0.25"
+                  d="M10 14.7611C12.6308 14.7611 14.7635 12.6284 14.7635 9.99763C14.7635 7.36682 12.6308 5.23413 10 5.23413C7.3692 5.23413 5.23651 7.36682 5.23651 9.99763C5.23651 12.6284 7.3692 14.7611 10 14.7611Z"
+                  fill="#D61116"></path>
+          </g>
+          <defs>
+            <clipPath id="clip0_832_5029">
+              <rect width="20" height="20" fill="white"></rect>
+            </clipPath>
+          </defs>
+        </LiveButtonBadge>
+        Live
+      </A>
+    </LiveButtonWrapper>
   );
 }
 
